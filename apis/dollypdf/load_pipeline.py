1817d3c48deb9cbb41bce1bd863e61686932a692
DoxB <jg.doxb@gmail.com>
2023-05-26 17:41:39 +0900

    pipeline 로컬 모델 로드 오류 수정

--- apis/dollypdf/load_pipeline.py
+++ apis/dollypdf/load_pipeline.py
@@ -2,7 +2,8 @@ import torch
 from transformers import pipeline
 
 # use dolly-v2-12b if you're using Colab Pro+, using pythia-2.8b for Free Colab
-generate_text = pipeline(model="../trained_models/alpaca-lora-dolly-2.0", 
+
+generate_text = pipeline('alpaca-lora-dolly-2.0', model="../../trained_models/alpaca-lora-dolly-2.0", 
                          torch_dtype=torch.bfloat16, 
                          trust_remote_code=True,
                          device_map="auto")
